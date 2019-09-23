287cc5d5cb80459f0c411fbf07f70bbe9ba34c0a
DmitryPukhov <dmitry.pukhov@gmail.com>
2019-09-23 09:05:58 +0300

    Single magic index with O(log(n)) implemented.

--- pyzuiz/dynamic/MagicIndex.py
+++ pyzuiz/dynamic/MagicIndex.py
@@ -1,3 +1,6 @@
+import math
+
+
 class MagicIndex:
     """
     8.3
@@ -9,8 +12,92 @@ class MagicIndex:
     Hints:#770, #204, #240, #286, #340
     """
 
+    def single(self, arr: []):
+        """
+        If the values are distinct, array can contain the only magic index
+        O(log(n)) implementation
+        :return: (start, end) of magic index
+         """
+        # Find an element inside magic index
+        inside_index = self._any_inside(arr)
+        if inside_index is None:
+            return None
+        start = self._start(arr, inside_index)
+        end = self._end(arr, inside_index)
+        return start, end
+
     @staticmethod
-    def of(arr):
+    def _start(arr, inside_index):
+        """
+
+        :param arr: given array
+        :param inside_index: index of element in array, which is inside magic index
+        :return: start of magic index
+        """
+        start = 0
+        end = inside_index
+        # Repeat until we narrow our search to lower bound
+        while start < end:
+            if arr[start] == start:
+                # We are inside index, move left
+                end = start
+                start = 0
+            else:
+                # We are before index, move right
+                start = start + math.ceil((end - start) / 2)
+        return start
+
+    @staticmethod
+    def _end(arr, inside_index):
+        """
+        :param arr: given array
+        :param inside_index: index of element in array, which is inside magic index
+        :return: end of magic index
+        """
+        max_end = len(arr) - 1
+        end = max_end
+        start = inside_index
+        # Repeat until we narrow our search to upper bound
+        while start < end:
+            if arr[end] == end:
+                # We are inside magic index, move right
+                start = end
+                end = max_end
+            else:
+                # We are after magic index, move left
+                end = start + math.floor((end - start) / 2)
+        return end
+
+    @staticmethod
+    def _any_inside(arr):
+        """
+        Find any index inside magic index
+        """
+        # Using binary division, find start of magic index
+        start = 0
+        end = len(arr)
+        # Find the element inside magic index
+        while start < end:
+            pivot = int(start + (end - start) / 2)
+            if arr[pivot] > pivot:
+                # We are on the right of magic index, move left
+                end = pivot
+            elif arr[pivot] < pivot:
+                # We are on the left of magic index, move right
+                start = pivot
+            else:
+                # We are inside magic index
+                return pivot
+        # Not found
+        return None
+
+    @staticmethod
+    def multi(arr: []):
+        """
+        O(n) implementation
+        If the values are not distinct, array can contain multiple magic indexes.
+        :return: array of tuples[(start1,end1),(start2,end2,)...]
+        """
         magic_indexes = []
         # Current magic index start and end
         mi_start = None
