53808dd44d6eba8ef33d8323192fbcc01b5cc141
DmitryPukhov <dmitry.pukhov@gmail.com>
2020-01-18 18:48:35 +0300

    DayOfTheWeek leetcode quiz implemented.

--- pyzuiz/leetcode/DayOfTheWeek.py
+++ pyzuiz/leetcode/DayOfTheWeek.py
@@ -1,89 +1,74 @@
 class DayOfTheWeek:
     """
-Given a date, return the corresponding day of the week for that date.
+    Given a date, return the corresponding day of the week for that date.
 
-The input is given as three integers representing the day, month and year respectively.
+    The input is given as three integers representing the day, month and year respectively.
 
-Return the answer as one of the following values {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}.
+    Return the answer as one of the following values {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}.
 
 
 
-Example 1:
+    Example 1:
 
-Input: day = 31, month = 8, year = 2019
-Output: "Saturday"
-Example 2:
+    Input: day = 31, month = 8, year = 2019
+    Output: "Saturday"
+    Example 2:
 
-Input: day = 18, month = 7, year = 1999
-Output: "Sunday"
-Example 3:
+    Input: day = 18, month = 7, year = 1999
+    Output: "Sunday"
+    Example 3:
 
-Input: day = 15, month = 8, year = 1993
-Output: "Sunday"
+    Input: day = 15, month = 8, year = 1993
+    Output: "Sunday"
     """
 
     def isleap(self, year: int):
-        if year % 400 == 0:
-            return True
-        elif year % 100 == 0:
+        """
+        if (year is not divisible by 4) then (it is a common year)
+        else if (year is not divisible by 100) then (it is a leap year)
+        else if (year is not divisible by 400) then (it is a common year)
+        else (it is a leap year)
+        :param year:
+        :return:
+        """
+        if year % 4 != 0:
             return False
-        elif year % 4 == 0:
+        elif year % 100 != 0:
             return True
-        return False
+        if year % 400 != 0:
+            return False
+        return True
+
+    def leaps(self, year):
+        """
+        Calc leap years up to given, excluding given
+        """
+        year -= 1
+        l0 = 1971 // 4 - (1971 // 100 - 1971 // 400)
+        return year // 4 - (year // 100 - year // 400) - l0
 
     def dayOfTheWeek(self, day: int, month: int, year: int) -> str:
-        day -= 1
-        month -= 1
 
-        # year days
-        yd = 0
-        for i in range(1, year):
-            yd += 365
-            if self.isleap(i):
-                yd += 1
+        # Start from 1971, so reset year
+        dyear = year - 1970
 
-        # month days
+        # Days in prev years
+        prevy = abs(dyear - 1)
+        l = self.leaps(year)
+        prevyd = prevy * 365 + l  # Compensate 1972 leap year
+
+        # Days in prev months of the year
         mdd = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
         if self.isleap(year):
             mdd[1] = 29
-        md = sum(mdd[0:month])
+        prevmd = sum(mdd[0:abs(month - 1)])
 
         # Total days
-        days = yd + md + day
+        days = prevyd + prevmd + day
 
         # Week day
-        wdays = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-        wd = days % 7
+        # From 1.1.1971
+        wdays = ["Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"]
+        wd = (days - 1) % 7
         return wdays[wd]
 
-
-# day = 1
-# month = 2
-# year = 2020
-# print(f'{day}.{month}.{year} is {DayOfTheWeek().dayOfTheWeek(day, month, year)}')
-
-
-# day = 1
-# month = 1
-# year = 2000
-# print(f'{day}.{month}.{year} is {DayOfTheWeek().dayOfTheWeek(day, month, year)}')
-#
-# day = 1
-# month = 21
-# year = 2001
-# print(f'{day}.{month}.{year} is {DayOfTheWeek().dayOfTheWeek(day, month, year)}')
-#
-# day = 31
-# month = 8
-# year = 2019
-# print(f'{day}.{month}.{year} is {DayOfTheWeek().dayOfTheWeek(day, month, year)}')
-#
-# day = 16
-# month = 1
-# year = 2020
-# print(f'{day}.{month}.{year} is {DayOfTheWeek().dayOfTheWeek(day, month, year)}')
-#
-day = 1
-month = 1
-year = 0
-print(f'{day}.{month}.{year} is {DayOfTheWeek().dayOfTheWeek(day, month, year)}')
--- test/leetcode/test_DayOfTheWeek.py
+++ test/leetcode/test_DayOfTheWeek.py
@@ -6,37 +6,44 @@ from pyzuiz.leetcode.DayOfTheWeek import DayOfTheWeek
 class TestDayOfTheWeek(TestCase):
     dw = DayOfTheWeek()
 
-    def test_isleap__notleap(self):
-        self.assertFalse(self.dw.isleap(1700))
-        self.assertFalse(self.dw.isleap(1800))
-        self.assertFalse(self.dw.isleap(1900))
-        self.assertFalse(self.dw.isleap(2100))
-        self.assertFalse(self.dw.isleap(2200))
-        self.assertFalse(self.dw.isleap(2300))
+    def test_leaps(self):
+        # Test how many leaps passed since 1972
+        self.assertEqual(0, self.dw.leaps(1971))
+        self.assertEqual(0, self.dw.leaps(1972))
+        self.assertEqual(1, self.dw.leaps(1973))
 
-    def test_isleap__leap(self):
-        self.assertTrue(self.dw.isleap(1600))
+        self.assertEqual(2, self.dw.leaps(1977))
+        self.assertEqual(3, self.dw.leaps(1981))
+        self.assertEqual(4, self.dw.leaps(1985))
+        self.assertEqual(5, self.dw.leaps(1989))
+        self.assertEqual(6, self.dw.leaps(1993))
+        self.assertEqual(7, self.dw.leaps(1997))
+        self.assertEqual(8, self.dw.leaps(2001))
+        self.assertEqual(9, self.dw.leaps(2005))
+        self.assertEqual(10, self.dw.leaps(2009))
+        self.assertEqual(11, self.dw.leaps(2013))
+
+    def test_isleap(self):
+        self.assertFalse(self.dw.isleap(1971))
+        self.assertTrue(self.dw.isleap(1972))
         self.assertTrue(self.dw.isleap(2000))
+        self.assertFalse(self.dw.isleap(2100))
+        self.assertTrue(self.dw.isleap(2400))
 
     def test_day_of_the_week(self):
-        self.assertEqual("Saturday", self.dw.dayOfTheWeek(1, 1, 1))
-        self.assertEqual("Saturday", self.dw.dayOfTheWeek(31, 12, 1))
-        self.assertEqual("Sunday", self.dw.dayOfTheWeek(1, 1, 2))
+        self.assertEqual("Friday", self.dw.dayOfTheWeek(1, 1, 1971))
+        self.assertEqual("Saturday", self.dw.dayOfTheWeek(1, 1, 1972))
+        self.assertEqual("Tuesday", self.dw.dayOfTheWeek(29, 2, 1972))
+        self.assertEqual("Wednesday", self.dw.dayOfTheWeek(1, 3, 1972))
+        self.assertEqual("Sunday", self.dw.dayOfTheWeek(31, 12, 1972))
+        self.assertEqual("Monday", self.dw.dayOfTheWeek(1, 1, 1973))
+        self.assertEqual("Friday", self.dw.dayOfTheWeek(31, 12, 1999))
+        self.assertEqual("Saturday", self.dw.dayOfTheWeek(1, 1, 2000))
+        self.assertEqual("Sunday", self.dw.dayOfTheWeek(31, 12, 2000))
+        self.assertEqual("Monday", self.dw.dayOfTheWeek(1, 1, 2001))
+        self.assertEqual("Saturday", self.dw.dayOfTheWeek(18, 1, 2020))
 
-        self.assertEqual("Wednesday", self.dw.dayOfTheWeek(31, 12, 4))
-        self.assertEqual("Thursday", self.dw.dayOfTheWeek(1, 1, 5))
+        self.assertEqual("Saturday", self.dw.dayOfTheWeek(31, 12, 2067))
 
-        self.assertEqual("Tuesday", self.dw.dayOfTheWeek(31, 12, 20))
-
-        # Bad
-        self.assertEqual("Thursday", self.dw.dayOfTheWeek(31, 12, 100))
-
-        # Bad
-        #self.assertEqual("Friday", self.dw.dayOfTheWeek(31, 12, 101))
-
-        # self.assertEqual("Friday", self.dw.dayOfTheWeek(31, 12, 1999))
-        # self.assertEqual("Saturday", self.dw.dayOfTheWeek(1, 1, 2000))
-
-
-        # self.assertEqual("Wednesday", self.dw.dayOfTheWeek(31, 12, 4))
-        # self.assertEqual("Thursday", self.dw.dayOfTheWeek(1, 1, 5))
+        self.assertEqual("Monday", self.dw.dayOfTheWeek(31, 12, 2068))
+        self.assertEqual("Tuesday", self.dw.dayOfTheWeek(1, 1, 2069))
