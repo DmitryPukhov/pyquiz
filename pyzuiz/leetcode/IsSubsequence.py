10ae00ea2ccb70f17c06e58f2274921986790ea5
DmitryPukhov <dmitry.pukhov@gmail.com>
2020-02-08 02:57:26 +0300

    IsSubsequence quiz

---
+++ pyzuiz/leetcode/IsSubsequence.py
@@ -0,0 +1,16 @@
+class IsSubsequence:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        pt1 = 0
+        for ss in s:
+            found = False
+            for pt in range(pt1,len(t)):
+                if t[pt] == ss:
+                    # Found
+                    found = True
+                    pt1 = pt
+                    break
+
+            if not found:
+                # Didn't find current letter
+                return False
+        return True
