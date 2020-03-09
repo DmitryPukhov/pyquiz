3198391888b2059c50c5e736154d7f37422ecc6f
Dmitry Pukhov <dmitry.pukhov@gmail.com>
2020-03-09 16:04:44 +0300

    Last Stone Weight quiz implemented

---
+++ test/leetcode/test_LastStoneWeight.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from pyzuiz.leetcode.LastStoneWeight import LastStoneWeight
+
+
+class TestLastStoneWeight(TestCase):
+
+    def test_solution(self):
+        alg = LastStoneWeight()
+        self.assertEqual(1, alg.solve([2, 7, 4, 1, 8, 1]))
