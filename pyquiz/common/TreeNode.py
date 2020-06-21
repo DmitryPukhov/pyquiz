239a2463e126d146da1998ec7a5920f858e23d3b
Dmitry Pukhov <dmitry.pukhov@gmail.com>
2020-06-21 10:07:11 +0300

    MergeTrees solved

--- pyquiz/common/TreeNode.py
+++ pyquiz/common/TreeNode.py
@@ -3,6 +3,11 @@ class TreeNode:
     Binary tree node with left and right child
     """
 
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
     def __init__(self, val, parent=None, left=None, right=None):
         self.val = val
         self.parent = parent
