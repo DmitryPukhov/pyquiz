81be7d0afbde2cb31d55431543ef6ab8efef6cc2
Dmitry Pukhov <dmitry.pukhov@gmail.com>
2020-08-21 11:45:47 +0300

    BFS traversal algo

---
+++ pyquiz/algo/tree/BFSTraversal.py
@@ -0,0 +1,26 @@
+from pyquiz.common.TreeNode import TreeNode
+
+
+class BFSTraversal:
+    """
+    Traverse binary tree using BFS - Breadth first search algorithm
+    """
+
+    def bfs(self, root: TreeNode) -> []:
+        """
+        Traverse the tree using BFS, return values
+        """
+        if not root:
+            return []
+        out = []
+        q = [root]
+        node = None
+        while q:
+            node = q.pop(0)
+            out.append(node.val)
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+
+        return out
