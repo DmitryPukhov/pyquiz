be04a68cdef7411b699aeaab88c9023e87d60fe5
Dmitry Pukhov <dmitry.pukhov@gmail.com>
2021-03-19 09:41:11 +0300

    RegularExpressionMatching solved

---
+++ pyquiz/leetcode/RegularExpressionMatching.py
@@ -0,0 +1,57 @@
+class Solution:
+    """
+    Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*' where:
+    '.' Matches any single character.​​​​
+    '*' Matches zero or more of the preceding element.
+    The matching should cover the entire input string (not partial).
+
+    Example 1:
+    Input: s = "aa", p = "a"
+    Output: false
+    Explanation: "a" does not match the entire string "aa".
+
+    Example 2:
+    Input: s = "aa", p = "a*"
+    Output: true
+    Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+
+    Example 3:
+    Input: s = "ab", p = ".*"
+    Output: true
+    Explanation: ".*" means "zero or more (*) of any character (.)".
+
+    Example 4:
+    Input: s = "aab", p = "c*a*b"
+    Output: true
+    Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore, it matches "aab".
+
+    Example 5:
+    Input: s = "mississippi", p = "mis*is*p*."
+    Output: false
+
+    Constraints:
+    0 <= s.length <= 20
+    0 <= p.length <= 30
+    s contains only lowercase English letters.
+    p contains only lowercase English letters, '.', and '*'.
+    It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.
+    """
+
+    def isMatch(self, s: str, p: str) -> bool:
+        if not p:
+            return not s
+        if not s:
+            if len(p) >= 2 and p[:2] == '.*':
+                return self.isMatch(s, p[2:])
+            else:
+                return False
+
+        is_first_match = p[0] == s[0] or p[0] == '.'
+        is_keeny_match = len(p) >= 2 and p[1] == '*'
+        if is_keeny_match:
+            # If wildcard in pattern
+            # Move s or pattern to the left and match again
+            return self.isMatch(s, p[2:]) or self.isMatch(s[1:], p) or self.isMatch(s[1:], p[2:])
+        else:
+            # No wildcard in pattern,
+            return is_first_match and self.isMatch(s[1:], p[1:])
---
+++ test/leetcode/test_RegularExpressionMatching.py
@@ -0,0 +1,79 @@
+from unittest import TestCase
+from pyquiz.leetcode.RegularExpressionMatching import Solution
+
+
+class TestSolution(TestCase):
+    def test_is_match__example1(self):
+        """
+        Example 1:
+        Input: s = "aa", p = "a"
+        Output: false
+        Explanation: "a" does not match the entire string "aa".
+        """
+        self.assertFalse(Solution().isMatch(s="aa", p="a"))
+
+    def test_is_match__example2(self):
+        """
+        Example 2:
+        Input: s = "aa", p = "a*"
+        Output: true
+        Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+        """
+        self.assertTrue(Solution().isMatch(s="aa", p="a*"))
+
+    def test_is_match__example3(self):
+        """
+        Example 3:
+        Input: s = "ab", p = ".*"
+        Output: true
+        Explanation: ".*" means "zero or more (*) of any character (.)".
+        """
+        self.assertTrue(Solution().isMatch(s="ab", p=".*"))
+
+    def test_is_match__example4(self):
+        """
+        Example 4:
+        Input: s = "aab", p = "c*a*b"
+        Output: true
+        Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore, it matches "aab".
+        """
+        self.assertTrue(Solution().isMatch(s="aab", p="c*a*b"))
+
+    def test_is_match__example5(self):
+        """
+        Example 5:
+        Input: s = "mississippi", p = "mis*is*p*."
+        Output: false
+        """
+        self.assertTrue(Solution().isMatch("mississippi", p="mis*is*p*."))
+
+    def test_is_match__ab_dotmany(self):
+        self.assertTrue(Solution().isMatch("ab", p=".*"))
+
+    def test_is_match__abc_dotmanyc(self):
+        self.assertTrue(Solution().isMatch("abc", p=".*c"))
+
+    def test_is_match__abc_dotmanyd(self):
+        self.assertFalse(Solution().isMatch("abc", p=".*d"))
+
+    def test_is_match__acb_dotmanybmany(self):
+        self.assertTrue(Solution().isMatch("accb", p=".*b*"))
+
+    def test_is_match__a_dotmanydotmany(self):
+        self.assertTrue(Solution().isMatch("a", p=".*.*"))
+
+    def test_is_match__ab_amanydot(self):
+        """
+        Example 5:
+        Input: s = "mississippi", p = "mis*is*p*."
+        Output: false
+        """
+        self.assertTrue(Solution().isMatch("a", p=".*."))
+
+    def test_is_match__ab_amanydot2(self):
+        """
+        Example 5:
+        Input: s = "mississippi", p = "mis*is*p*."
+        Output: false
+        """
+        self.assertTrue(Solution().isMatch("abc", p=".*."))
